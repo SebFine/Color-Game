0010717c45b0cbe11fb8b1b17c7edf241deb036d
62475734 <62475734+SebFine@users.noreply.github.com>
2020-05-12 08:46:16 +0200

    update 12.05.20

--- appAlong.js
+++ appAlong.js
@@ -3,22 +3,24 @@ const squares = document.querySelectorAll(".square");
 const colorDisplay = document.querySelector("#rgb");
 const displayResult = document.querySelector("#result");
 const header = document.querySelector(".header");
+const reset = document.querySelector("#reset");
 const pickedColor = pickColor();
 
 function getRandomRgb() {
     const arr = [];
-    for (i=0; i<6; i++) {
+    difficulty();
+    for (i=0; i<mode; i++) {
     colorToGuessArr = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
     colorToGuessStr = `rgb(${colorToGuessArr.join(", ")})`;
     arr.push(colorToGuessStr);
     }
     return arr;
-}
+};
 
 function pickColor() {
 	const random = Math.floor(Math.random() * colors.length);
 	return colors[random];
-}
+};
 
 colorDisplay.textContent = pickedColor;
 
@@ -36,9 +38,8 @@ for (i = 0; i < squares.length; i++) {
             this.style.backgroundColor = "#232323";
             displayResult.textContent = ("Try Again!");
         }
-
-    });
-}
+    })
+};
 
 function changeColors(color) {
 	//loop through all squares
@@ -51,4 +52,28 @@ function changeColors(color) {
 function pickColor() {
 	const random = Math.floor(Math.random() * colors.length);
 	return colors[random];
-}
+};
+
+// Try again button. Working but not good as it´s making a new request every time.
+reset.addEventListener("click", function() {
+    location.reload();
+    return false;
+});
+
+// Difficulty settings
+easyBtn.addEventListener("click", difficulty(easy));
+hardBtn.addEventListener("click", difficulty(hard));
+
+function difficulty() {
+    const modeBtn = document.querySelectorAll(".mode");
+    let mode = 6;
+    for(var i = 0; i < modeBtn.length; i++) {
+        modeBtn[i].addEventListener("click", function() {
+            modeBtn[0].classList.remove("active");
+            modeBtn[1].classList.remove("active");
+            this.classList.add("active");	
+            this.textContent === "Easy" ?	mode = 3 : mode = 6;   
+        });
+    }
+    return mode;
+};
